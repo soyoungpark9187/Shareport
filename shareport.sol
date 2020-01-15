2c7000c4a3c60683193d647a476c8ee8e356a871
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-01-15 18:49:40 +0900

    solidity code

---
+++ shareport.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.0;
+
+contract Shareport {
+
+address writer;
+address buyer;
+uint score;
+string text;
+uint storedData;
+
+struct report {
+    address writer;
+    address buyer;
+    string text;
+    uint score;
+}
+
+// array of reports
+report[] public reports;
+
+    function setReview (address _writer, address _buyer, string _text, uint _score) {
+        reports.push(
+            report({
+                writer: _writer,
+                buyer: _buyer,
+                text: _text,
+                score: _score
+            })
+        );
+    }
+        
+    function set(uint x) {
+        storedData = x;
+    }
+
+    function get() constant returns (uint) {
+        return storedData;
+    }
+
+}
